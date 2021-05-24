2d7b66c83612812b4f6793139b8ddf9b9f466de1
Naveena0607 <surakantinaveenareddy@gmail.com>
2021-05-24 12:46:16 +0530

    Task1

---
+++ app/src/main/java/com/example/task1/link_to_others.java
@@ -0,0 +1,14 @@
+package com.example.task1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class link_to_others extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_link_to_others);
+    }
+}
---
+++ app/src/main/java/com/example/task1/paymentsuccessfull.java
@@ -0,0 +1,14 @@
+package com.example.task1;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class paymentsuccessfull extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_paymentsuccessfull);
+    }
+}
